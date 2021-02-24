6d8c9ac0d726204a4d4c046369898774a237c64f
morisummer2 <79527804+morisummer2@users.noreply.github.com>
2021-02-25 02:58:54 +0900

    Create 065. 정수 1개 입력받아 분석하기.cpp

---
+++ 065. 정수 1개 입력받아 분석하기.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+
+	int a;
+	cin >> a;
+
+	if (a < 0)
+		cout << "minus" << endl;
+	else
+		cout << "plus" << endl;
+
+	if (a % 2 == 0)
+		cout << "even" << endl;
+	else
+		cout << "odd" << endl;
+
+}
