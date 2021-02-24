6c8337e432c3665ab431719ed345d7f7bbcb806a
morisummer2 <79527804+morisummer2@users.noreply.github.com>
2021-02-25 03:02:53 +0900

    Create 021. 실수 1개 입력받아 부분별로 출력하기.cpp

---
+++ 021. 실수 1개 입력받아 부분별로 출력하기.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	int a,b;
+	scanf("%d.%d", &a, &b);
+
+	cout << a << endl;
+	cout << b << endl;
+	return 0;
+
+}
