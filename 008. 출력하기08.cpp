c812e06a08c7abb8697dea182e8a877d02faee51
morisummer2 <79527804+morisummer2@users.noreply.github.com>
2021-02-25 03:04:01 +0900

    Create 008. 출력하기08.cpp

---
+++ 008. 출력하기08.cpp
@@ -0,0 +1,15 @@
+//┌┬┐
+//├┼┤	출력하기
+//└┴┘
+
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+
+	cout << "\u250c\u252c\u2510" << endl;
+	cout << "\u251c\u253c\u2524" << endl;
+	cout << "\u2514\u2534\u2518" << endl;
+
+}
