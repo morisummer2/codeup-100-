3dd35d17c941903f0aa60328343f75f8d968fb8b
morisummer2 <79527804+morisummer2@users.noreply.github.com>
2021-02-25 02:57:48 +0900

    Create 076. 짝수 합 구하기.cpp

---
+++ 076. 짝수 합 구하기.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+
+	int a;
+	int sum =0;
+	cin >> a;
+	for (int i = 1; i <= a; i++)
+		if (i % 2 == 0)
+			sum += i;
+
+	cout << sum << endl;
+}
